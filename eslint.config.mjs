5af26083fbbbfd21abd9f29f4c5eda64581c1d83
Birat Khanal <biratkhanal4@gmail.com>
2026-05-21 16:46:44 +0545

    initial setup: Next.js with Prettier, ESLint, Husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";  
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+    prettier,  // t ooverride othe rules
+
+   {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  
 ]);
 
 export default eslintConfig;
